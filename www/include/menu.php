dea8a58e15503d47b1f96535187104de261a00d7
Bernat <bernat.costa@cesurformacion.com>
2021-03-08 00:11:50 +0100

    ayuda

--- www/include/menu.php
+++ www/include/menu.php
@@ -6,12 +6,11 @@
 <nav class="navbar navbar-dark bg-dark">
 <div class="container">
     <div class="row">
-      
             <div class="col-3-md">
                 <a href="index.php" class="navbar-brand">Inicio</a>
             </div>
             <div class="col-3-md">
-                <a href="index.php" class="navbar-brand">Ayuda</a>
+                <a href="./help/info.php" class="navbar-brand">Ayuda</a>
             </div>
         </div>
     </div>

8fb40bc85bee75ccde3668bbd78d502d0ce95a06
Bernat <bernat.costa@cesurformacion.com>
2021-03-08 00:08:00 +0100

    cabecera y footer

---
+++ www/include/menu.php
@@ -0,0 +1,18 @@
+<header class="navbar navbar-dark bg-dark">
+<div class="container">
+ <h1 class="text-white">Mi Agenda</h1>
+</div>
+</header>
+<nav class="navbar navbar-dark bg-dark">
+<div class="container">
+    <div class="row">
+      
+            <div class="col-3-md">
+                <a href="index.php" class="navbar-brand">Inicio</a>
+            </div>
+            <div class="col-3-md">
+                <a href="index.php" class="navbar-brand">Ayuda</a>
+            </div>
+        </div>
+    </div>
+</nav>
--- www/index.php
+++ www/index.php
@@ -1,39 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Mi agenda</title>
-    <!-- CDN de Bootstrap 4-->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <!--Fontasome-->
-    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
-        integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <script>
-        function validarform() {
-            var x = document.forms["form"]["id"].value;
-            if (x == "") {
-                alert("El campo de id no puede ir vacio");
-                return false;
-            }
-        }
-    </script>
-</head>
-
+<html lang="es">
+<?php include("./include/head.php");?>
 <body>
+<?php include("./include/menu.php");?>
     <div class="container">
-        <nav class="navbar navbar-dark bg-dark">
-            <div class="container">
-                <a href="index.php" class="navbar-brand">Mi Agenda</a>
-            </div>
-        </nav>
         <div class="container p-4">
             <?php include("conexion.php");?>
             <div class="row">
-                <div class="col-md-4 mx-auto">
+                <aside class="col-md-4 mx-auto">
                     <div class="card card-body">
                         <form method="post" name="form" onsubmit="return validarform()" action="create.php">
                             <div class="form-group">
@@ -56,8 +30,8 @@
                             <input type="reset" class="btn btn-secondary btn-block" value="Limpiar">
                         </form>
                     </div>
-                </div> <!--End col-md-4-->
-                <div class="col-md-8 mx-auto">
+                </aside> <!--End col-md-4-->
+                <article class="col-md-8 mx-auto">
                     <table class="table table-hover">
                         <thead>
                             <tr>
@@ -92,20 +66,12 @@
                             <?php } ?>
                         </tbody>
                     </table>
-                </div> <!--End col-md-8-->
+                </article> <!--End col-md-8-->
             </div> <!--End row-->
         </div><!--End container p-4-->
     </div><!--End container-->
-    <!--Scripts-->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-        crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-        crossorigin="anonymous"></script>
+    <?php include("./include/pie.php");?>
+  
 </body>
 
 </html>
